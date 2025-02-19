331c8e2021bc99e2302a1faa2e895687e203f3b5
Enkel <enkel.osmani1@gmail.com>
2025-02-19 02:08:27 +0100

    New

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\ContentController;
 use App\Http\Controllers\LikeController;
@@ -10,13 +11,21 @@ use Illuminate\Support\Facades\Route;
 //Route::get('/user', function (Request $request) {
 //    return $request->user();
 //})->middleware('auth:sanctum');
+Route::post('login',[AuthController::class,'login']);
+Route::post('logout',[AuthController::class,'logout']);
 
-Route::apiResource('posts',PostController::class);
-Route::post('/posts/{post}/likes', [LikeController::class, 'store']);
-Route::delete('/posts/{post}/likes', [LikeController::class, 'destroy']);
-Route::post('posts/{post}/comments',[CommentController::class , 'store']);
-Route::apiResource('contents',ContentController::class);
-Route::apiResource('users',UserController::class);
+
+Route::middleware(['auth:api'])->group( function () {
+    Route::post('logout',[AuthController::class,'logout']);
+    Route::apiResource('posts', PostController::class);
+    Route::post('/posts/{post}/likes', [LikeController::class, 'store']);
+    Route::delete('/posts/{post}/likes', [LikeController::class, 'destroy']);
+    Route::post('posts/{post}/comments', [CommentController::class, 'store']);
+
+    Route::apiResource('contents', ContentController::class);
+    Route::apiResource('users', UserController::class);
+
+});
 
 
 
